624802633aaa9b005f791c91cf54e4dde0ee94c8
ankiwoong <ankiwoong@gmail.com>
2022-05-22 20:54:08 +0900

    modify

--- build/hello.js
+++ build/hello.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const os = require('os');
 
-var handler = function(request, response) {
+var handler = function (request, response) {
 	console.log("Received request from " +
-	request.connection.remoteAddress);
+		request.connection.remoteAddress);
 	response.writeHead(200);
 	response.end("Containser Hostname: " + os.hostname() + "\n");
 };
